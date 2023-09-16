4de2eeff68f208110631119feac14d9fce3e6ad4
ryanata <ataryan47@gmail.com>
2023-09-16 00:52:00 -0400

    Added homepage template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import TemplateCard from "./TemplateCard.svelte";
+</script>
+
+<svelte:head>
+    <title>Resumex | AI Latex Resume Maker</title>
+	<meta name="description" content="Create and modify your resumes with generative AI" />
+</svelte:head>
+
+<section class="centered-container">
+    <h1>insert image here</h1>
+    <p>This is a small description of the product. It's the best product you'll ever find!</p>
+    <div class="templates-container">
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+        <TemplateCard />
+    </div>
+</section>
+
+<style>
+    .centered-container {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .templates-container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        gap: 10px;
+    }
+</style>
---
+++ src/routes/TemplateCard.svelte
@@ -0,0 +1,34 @@
+<div class="template-card">
+    <div class="image-container">
+        <div class="image">image here</div>
+    </div>
+    <div class="text-container">
+        <p>Simple</p>
+    </div>
+</div>
+
+<style>
+    .template-card {
+        display: flex;
+        flex-direction: column;
+        background-color: #D9D9D9;
+        width: 200px;
+        height: 225px;
+    }
+    .image-container {
+        display: flex;
+        flex: 1;
+        justify-content: center;
+        align-items: center;
+    }
+    .image {
+        background-color: white;
+        width: 80%;
+        height: 80%;
+        border-radius: 10px;
+    }
+    .text-container {
+        display: flex;
+        flex: 1;
+    }
+</style>
