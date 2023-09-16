13981a47da70738fa2a185ebd69d38465af04ce2
Camila Sinelli <camilasinelli@gmail.com>
2023-09-16 03:00:41 -0400

    Added cards to start page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import TemplateCard from "./TemplateCard.svelte";
+    import imgLink from "./TemplateCard.svelte";
 </script>
 
 <svelte:head>
@@ -9,9 +10,9 @@
 
 <section class="centered-container">
     <h1>insert image here</h1>
-    <p>This is a small description of the product. It's the best product you'll ever find!</p>
+    <p>Craft your latex resume with the power of generative AI 😁</p>
     <div class="templates-container">
-        <TemplateCard />
+        <TemplateCard imgLink="https://writelatex.s3.amazonaws.com/published_ver/30280.jpeg?X-Amz-Expires=14400&X-Amz-Date=20230916T061630Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWJBOALPNFPV7PVH5/20230916/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=8b90baaee69887502dbab6bec66239e18dfc3ee8aec37a540046792a664aec7d"/>
         <TemplateCard />
         <TemplateCard />
         <TemplateCard />
@@ -23,6 +24,9 @@
 </section>
 
 <style>
+    :root{
+        background-color: #F9F8FD;
+    }
     .centered-container {
         width: 100%;
         height: 100vh;
@@ -35,6 +39,6 @@
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         grid-template-rows: repeat(2, 1fr);
-        gap: 10px;
+        gap: 30px;
     }
 </style>
--- src/routes/TemplateCard.svelte
+++ src/routes/TemplateCard.svelte
@@ -1,9 +1,13 @@
+<script>
+    export let imgLink;
+</script>
+
 <div class="template-card">
     <div class="image-container">
-        <div class="image">image here</div>
+        <div class="image"><img class="picture" src={imgLink} alt="Preview of template"></div>
     </div>
     <div class="text-container">
-        <p>Simple</p>
+        <p class="text">Simple</p>
     </div>
 </div>
 
@@ -11,24 +15,43 @@
     .template-card {
         display: flex;
         flex-direction: column;
-        background-color: #D9D9D9;
+        background-color: white;
         width: 200px;
         height: 225px;
+        border-radius: 5px;
+        box-shadow: 0 2px 2px 0 rgba(46, 46, 46, 0.19);
     }
+    .template-card:hover {
+        cursor: pointer;
+        background-color: #F4FAFE;
+    }
+    .template-card:active {
+        background-color: #EAF2F8;
+    }
+
     .image-container {
         display: flex;
-        flex: 1;
+        flex: 2;
         justify-content: center;
         align-items: center;
     }
     .image {
-        background-color: white;
-        width: 80%;
-        height: 80%;
+        background-color: #f2f2f2;
+        width: 90%;
+        height: 85%;
+        border-radius: 10px;
+    }
+    .picture {
+        width: 100%;
+        max-height: 100%;
         border-radius: 10px;
     }
     .text-container {
         display: flex;
+        padding-left: 10px;
         flex: 1;
+    }
+    .text {
+        margin-top: 0px;
     }
 </style>
